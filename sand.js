eadc0ab133b1cc67d0eba90a90ce9bf06baa6f11
Sophie Kirschner <sophie@thehumangeo.com>
2017-09-17 23:54:23 -0400

    Initial code commit

---
+++ sand.js
@@ -0,0 +1,595 @@
+const elements = {
+    void: {
+        id: 0,
+        red: 0, green: 0, blue: 0,
+        density: 0, gravity: 0, slip: 0,
+        reactions: [],
+    },
+    wall: {
+        id: 1,
+        red: 0.5, green: 0.5, blue: 0.5,
+        density: 1, gravity: 0, slip: 0,
+        immobile: true,
+        reactions: [],
+    },
+    sand: {
+        id: 2,
+        red: 1.0, green: 0.8, blue: 0.2,
+        density: 0.7, gravity: 0.8, slip: 0.1,
+        reactions: [],
+    },
+    fire: {
+        id: 3,
+        red: 0.9, green: 0.2, blue: 0.1,
+        density: -0.5, gravity: -0.4, slip: 0.3,
+        reactions: [],
+    },
+    torch: {
+        id: 3,
+        red: 0.4, green: 0.1, blue: 0,
+        density: 1, gravity: 0, slip: 0,
+        immobile: true,
+        reactions: [],
+    },
+    water: {
+        id: 7,
+        red: 0, green: 0.4, blue: 1,
+        density: 0.5, gravity: 0.8, slip: 0.8,
+        reactions: [],
+    },
+    plant: {
+        id: 4,
+        red: 0.2, green: 0.8, blue: 0,
+        density: 1, gravity: 0, slip: 0,
+        immobile: true,
+        reactions: [],
+    },
+    growingPlantHead: {
+        id: 5,
+        red: 0.2, green: 0.8, blue: 0, //red: 0.2, green: 1, blue: 0,
+        density: 1, gravity: 0, slip: 0,
+        immobile: true,
+        reactions: [],
+        hidden: true,
+    },
+    growingPlantTail: {
+        id: 6,
+        red: 0.2, green: 0.8, blue: 0, //red: 0.2, green: 0.9, blue: 0,
+        density: 1, gravity: 0, slip: 0,
+        immobile: true,
+        reactions: [],
+        hidden: true,
+    },
+};
+
+elements.fire.reactions.push({
+    self: true,
+    chance: 0.25,
+    becomes: elements.void,
+});
+
+elements.fire.reactions.push({
+    chance: 0.55,
+    becomes: elements.void,
+    singleNeighbor: {
+        element: elements.void,
+        becomes: elements.fire,
+        minimum: 1,
+        maximum: 6,
+        affects: 1,
+    },
+});
+
+elements.fire.reactions.push({
+    chance: 0.85,
+    becomes: elements.void,
+    singleNeighbor: {
+        element: elements.water,
+        becomes: elements.water,
+        minimum: 1,
+        maximum: 8,
+        affects: 0,
+    },
+});
+
+elements.torch.reactions.push({
+    chance: 0.8,
+    becomes: elements.torch,
+    singleNeighbor: {
+        element: elements.void,
+        becomes: elements.fire,
+        minimum: 1,
+        maximum: 6,
+        affects: 2,
+    },
+});
+
+elements.plant.reactions.push({
+    chance: 0.6,
+    becomes: elements.fire,
+    singleNeighbor: {
+        element: elements.fire,
+        becomes: elements.fire,
+        minimum: 2,
+        maximum: 8,
+        affects: 0,
+    },
+});
+
+elements.plant.reactions.push({
+    chance: 0.5,
+    becomes: elements.growingPlantHead,
+    singleNeighbor: {
+        element: elements.water,
+        becomes: elements.growingPlantHead,
+        minimum: 2,
+        maximum: 8,
+        affects: 1,
+    },
+});
+
+elements.growingPlantHead.reactions.push({
+    chance: 0.75,
+    becomes: elements.growingPlantHead,
+    singleNeighbor: {
+        element: elements.plant,
+        becomes: elements.growingPlantHead,
+        minimum: 1,
+        maximum: 8,
+        affects: 2,
+    },
+});
+
+elements.growingPlantHead.reactions.push({
+    chance: 0.1,
+    becomes: elements.growingPlantTail,
+    singleNeighbor: {
+        element: elements.water,
+        becomes: elements.growingPlantHead,
+        minimum: 1,
+        maximum: 8,
+        affects: 3,
+    },
+});
+
+elements.growingPlantHead.reactions.push({
+    chance: 0.6,
+    becomes: elements.growingPlantHead,
+    singleNeighbor: {
+        element: elements.water,
+        becomes: elements.void,
+        minimum: 1,
+        maximum: 8,
+        affects: 3,
+    },
+});
+
+elements.growingPlantHead.reactions.push({
+    chance: 0.4,
+    becomes: elements.growingPlantTail,
+    singleNeighbor: {
+        element: elements.sand,
+        becomes: elements.plant,
+        minimum: 1,
+        maximum: 8,
+        affects: 1,
+    },
+});
+
+elements.growingPlantHead.reactions.push({
+    chance: 0.1,
+    becomes: elements.growingPlantTail,
+    singleNeighbor: {
+        element: elements.wall,
+        becomes: elements.plant,
+        minimum: 1,
+        maximum: 8,
+        affects: 1,
+    },
+});
+
+elements.growingPlantHead.reactions.push({
+    self: true,
+    chance: 0.6,
+    becomes: elements.growingPlantTail,
+});
+
+elements.growingPlantTail.reactions.push({
+    self: true,
+    chance: 0.015,
+    becomes: elements.plant,
+});
+
+const defaultElement = elements.void;
+
+function shuffleArray(array){
+    const newArray = [];
+    let i = 0;
+    for(const element of array){
+        let j = Math.floor(Math.random() * i);
+        if(i === j){
+            newArray.push(element);
+        }else{
+            newArray.push(newArray[j]);
+            newArray[j] = element;
+        }
+        i++;
+    }
+    return newArray;
+}
+
+function rgb2hex(red, green, blue){
+    return "#" + (
+        ("0" + Math.floor(red * 255).toString(16)).slice(-2) +
+        ("0" + Math.floor(green * 255).toString(16)).slice(-2) +
+        ("0" + Math.floor(blue * 255).toString(16)).slice(-2)
+    );
+}
+
+function nextPow2(n){
+    return Math.pow(2, Math.ceil(Math.log2(n)));
+}
+
+function newBuffer(width, height, fill){
+    const buffer = [];
+    buffer.width = width;
+    buffer.height = height;
+    for(let i = 0; i < width * height; i++){
+        buffer.push(fill);
+    }
+    return buffer;
+}
+
+function newTexture(gl, pixels){
+    return twgl.createTexture(gl, {
+        target: gl.TEXTURE_2D,
+        width: pixels.width,
+        height: pixels.height,
+        minMag: gl.NEAREST,
+        internalFormat: gl.RGB,
+        format: gl.RGB,
+        wrap: gl.CLAMP_TO_EDGE,
+        src: pixels,
+    });
+}
+
+function newPixelData(buffer){
+    const textureWidth = nextPow2(buffer.width);
+    const textureHeight = nextPow2(buffer.height);
+    const pixels = new Uint8Array(textureWidth * textureHeight * 3);
+    pixels.width = textureWidth;
+    pixels.height = textureHeight;
+    return blitPixelData(pixels, buffer);
+}
+
+function blitPixelData(pixels, buffer){
+    let j = 0;
+    for(let i = 0; i < buffer.length; i++){
+        pixels[j++] = Math.floor(buffer[i].red * 255);
+        pixels[j++] = Math.floor(buffer[i].green * 255);
+        pixels[j++] = Math.floor(buffer[i].blue * 255);
+        if(i % buffer.width === buffer.width - 1){
+            j += (pixels.width - buffer.width) * 3;
+        }
+    }
+    return pixels;
+}
+
+function updateBuffers(timestamp, timeBuffer, readBuffer, writeBuffer){
+    const neighbors = [
+        -readBuffer.width - 1,
+        -readBuffer.width,
+        -readBuffer.width + 1,
+        -1,
+        +1,
+        +readBuffer.width - 1,
+        +readBuffer.width,
+        +readBuffer.width + 1,
+    ];
+    const neighborOrders = [
+        shuffleArray(neighbors),
+        shuffleArray(neighbors),
+        shuffleArray(neighbors),
+        shuffleArray(neighbors),
+        shuffleArray(neighbors),
+        shuffleArray(neighbors),
+    ];
+    let neighborOrderIndex = 0;
+    const mode = timestamp % 2;
+    const increment = mode ? -1 : +1;
+    const totalCells = readBuffer.width * readBuffer.height;
+    let i = mode * (totalCells - 1);
+    let west = i - 1;
+    let east = i + 1;
+    let north = i - readBuffer.width;
+    let south = i + readBuffer.width;
+    let random = Math.random();
+    // for(let j = 0; j < readBuffer.length; j++){
+    //     writeBuffer[j] = readBuffer[j];
+    // }
+    while(i >= 0 && i < totalCells){
+        const cell = readBuffer[i];
+        if(!cell) console.log(i);
+        if(
+            cell.gravity > 0 &&
+            south < totalCells &&
+            timeBuffer[south] < timestamp &&
+            !readBuffer[south].immobile &&
+            readBuffer[south].density <= cell.density
+        ){
+            if(cell.gravity > random){
+                writeBuffer[i] = readBuffer[south];
+                writeBuffer[south] = cell;
+                timeBuffer[i] = timestamp;
+                timeBuffer[south] = timestamp;
+            }
+            random = Math.random();
+        }else if(
+            cell.gravity < 0 &&
+            north > 0 &&
+            timeBuffer[north] < timestamp &&
+            !readBuffer[north].immobile &&
+            readBuffer[north].density >= cell.density
+        ){
+            if(cell.gravity < -random){
+                writeBuffer[i] = readBuffer[north];
+                writeBuffer[north] = cell;
+                timeBuffer[i] = timestamp;
+                timeBuffer[north] = timestamp;
+                console.log("It fell up!?");
+            }
+            random = Math.random();
+        }else if(cell.slip > random){
+            const westAllowed = (
+                timeBuffer[west] < timestamp &&
+                !readBuffer[west].immobile &&
+                cell.density >= readBuffer[west].density
+            );
+            const eastAllowed = (
+                timeBuffer[east] < timestamp &&
+                !readBuffer[east].immobile &&
+                cell.density >= readBuffer[east].density
+            );
+            if(westAllowed && (!eastAllowed || Math.random() < 0.5)){
+                writeBuffer[i] = readBuffer[west];
+                writeBuffer[west] = cell;
+                timeBuffer[i] = timestamp;
+                timeBuffer[west] = timestamp;
+            }else if(eastAllowed){
+                writeBuffer[i] = readBuffer[east];
+                writeBuffer[east] = cell;
+                timeBuffer[i] = timestamp;
+                timeBuffer[east] = timestamp;
+            }
+            random = Math.random();
+        }
+        if(timeBuffer[i] < timestamp){
+            writeBuffer[i] = readBuffer[i];
+            if(cell.reactions.length){
+                const neighbors = [];
+                const northwest = north - 1;
+                const northeast = north + 1;
+                const southwest = south - 1;
+                const southeast = south + 1;
+                if(northwest > 0) neighbors[readBuffer[northwest].id] = (
+                    (neighbors[readBuffer[northwest].id] || 0) + 1
+                );
+                if(north > 0) neighbors[readBuffer[north].id] = (
+                    (neighbors[readBuffer[north].id] || 0) + 1
+                );
+                if(northeast > 0) neighbors[readBuffer[northeast].id] = (
+                    (neighbors[readBuffer[northeast].id] || 0) + 1
+                );
+                if(west > 0) neighbors[readBuffer[west].id] = (
+                    (neighbors[readBuffer[west].id] || 0) + 1
+                );
+                if(southwest > 0) neighbors[readBuffer[southwest].id] = (
+                    (neighbors[readBuffer[southwest].id] || 0) + 1
+                );
+                if(south > 0) neighbors[readBuffer[south].id] = (
+                    (neighbors[readBuffer[south].id] || 0) + 1
+                );
+                if(southeast > 0) neighbors[readBuffer[southeast].id] = (
+                    (neighbors[readBuffer[southeast].id] || 0) + 1
+                );
+                if(west > 0) neighbors[readBuffer[west].id] = (
+                    (neighbors[readBuffer[west].id] || 0) + 1
+                );
+                for(const reaction of cell.reactions){
+                    if(reaction.singleNeighbor && reaction.chance > random){
+                        const actual = neighbors[reaction.singleNeighbor.element.id];
+                        if(
+                            actual >= reaction.singleNeighbor.minimum &&
+                            actual <= reaction.singleNeighbor.maximum
+                        ){
+                            writeBuffer[i] = reaction.becomes;
+                            timeBuffer[i] = timestamp;
+                            const neighborIndexes = neighborOrders[neighborOrderIndex++];
+                            if(neighborOrderIndex >= neighborOrders.length){
+                                neighborOrderIndex = 0;
+                            }
+                            let changed = 0;
+                            for(let j = 0; j < 8 && changed < reaction.singleNeighbor.affects; j++){
+                                const index = i + neighborIndexes[j];
+                                if(
+                                    index >= 0 && index < readBuffer.length &&
+                                    readBuffer[index] === reaction.singleNeighbor.element
+                                ){
+                                    writeBuffer[index] = reaction.singleNeighbor.becomes;
+                                    timeBuffer[index] = timestamp;
+                                    changed++;
+                                }
+                            }
+                            break;
+                        }
+                    }else if(reaction.self && reaction.chance > random){
+                        writeBuffer[i] = reaction.becomes;
+                        timeBuffer[i] = timestamp;
+                    }
+                    random = Math.random()
+                }
+            }else if(cell.gravity){
+                random = Math.random();
+            }
+        }
+        i += increment;
+        north += increment;
+        south += increment;
+        east += increment;
+        west += increment;
+    }
+}
+
+const vertexShader = `
+    precision mediump float;
+    attribute vec2 a_position;
+    varying vec2 v_position;
+    void main(){
+        v_position = a_position;
+        gl_Position = vec4(
+            a_position.x * 2.0 - 1.0, v_position.y * -2.0 + 1.0, 0.0, 1.0
+        );
+    }
+`;
+
+const fragmentShader = `
+    precision mediump float;
+    uniform sampler2D u_texture;
+    varying vec2 v_position;
+    void main(){
+        gl_FragColor = texture2D(u_texture, v_position);
+    }
+`;
+
+function begin(){
+    const canvas = document.getElementById("glCanvas");
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    const gl = twgl.getWebGLContext(canvas, {
+        alpha: false,
+        antialias: false,
+        depth: false,
+        stencil: false,
+    });
+    twgl.setDefaults({
+        textureColor: [0, 0, 0, 1],
+        attribPrefix: "a_",
+    });
+    
+    gl.enable(gl.CULL_FACE);
+    gl.clearColor(0, 0, 0, 1);
+    
+    const bufferWidth = 128;
+    const bufferHeight = 128;
+    
+    let currentBuffer = newBuffer(bufferWidth, bufferHeight, defaultElement);
+    let reserveBuffer = newBuffer(bufferWidth, bufferHeight, defaultElement);
+    const timeBuffer = newBuffer(bufferWidth, bufferHeight, 0);
+    
+    const pixels = newPixelData(currentBuffer);
+    const texture = newTexture(gl, pixels);
+    
+    const programInfo = twgl.createProgramInfo(gl, [
+        vertexShader, fragmentShader,
+    ]);
+    gl.useProgram(programInfo.program);
+    twgl.setUniforms(programInfo, {
+        u_texture: texture,
+    });
+    
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, {
+        position: {
+            numComponents: 2, data: [0, 0, 0, 1, 1, 0, 1, 1]
+        },
+    });
+    
+    let frame = 0;
+    
+    // http://stackoverflow.com/questions/381795/how-to-disable-right-click-context-menu-in-javascript
+    canvas.addEventListener("contextmenu", function(event){
+        if(event.preventDefault) event.preventDefault();
+        if(event.stopPropagation) event.stopPropagation();
+        return false;
+    });
+    
+    let drawing = false;
+    let selectedElement = elements.wall;
+    
+    canvas.addEventListener("mousedown", function(event){
+        drawing = true;
+    }, false);
+    canvas.addEventListener("mouseup", function(event){
+        drawing = false;
+    }, false);
+    canvas.addEventListener("mousemove", function(event){
+        if(drawing){
+            const rect = canvas.getBoundingClientRect();
+            const x = (event.clientX - rect.left) / rect.width;
+            const y = (event.clientY - rect.top) / rect.height;
+            if(x >= 0 && y >= 0 && x < 1 && y < 1){
+                const i = Math.floor(x * bufferWidth);
+                const j = Math.floor(y * bufferHeight);
+                for(let brushX = -2; brushX < 2; brushX++){
+                    for(let brushY = -2; brushY < 2; brushY++){
+                        const drawX = i + brushX;
+                        const drawY = j + brushY;
+                        if(drawX >= 0 && drawY >= 0 && drawX < bufferWidth && drawY < bufferHeight){
+                            currentBuffer[drawX + drawY * bufferWidth] = selectedElement;
+                        }
+                    }
+                }
+            }
+        }
+    }, false);
+    
+    for(const elementName in elements){
+        const element = elements[elementName];
+        if(!element.hidden){
+            const button = document.createElement("div");
+            const label = document.createTextNode(elementName);
+            button.appendChild(label);
+            const hex = rgb2hex(element.red * 0.5, element.green * 0.5, element.blue * 0.5);
+            button.style = `
+                font-family: Sans-Serif;
+                display: inline-block;
+                margin: 8px;
+                width: 80px;
+                color: #fff;
+                background-color: ${hex};
+                outline-color: #fff;
+                outline-style: solid;
+                outline-width: 2px;
+                text-align: center;
+            `;
+            document.getElementById("elementButtons").appendChild(button);
+            button.onclick = function(event){
+                selectedElement = element;
+            }
+        }
+    }
+    
+    const update = () => {
+        // Update game state
+        updateBuffers(frame, timeBuffer, currentBuffer, reserveBuffer)
+        const tempBuffer = currentBuffer;
+        currentBuffer = reserveBuffer;
+        reserveBuffer = tempBuffer;
+        // Update texture
+        blitPixelData(pixels, currentBuffer);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texSubImage2D(
+            gl.TEXTURE_2D, 0, 0, 0, bufferWidth, bufferHeight,
+            gl.RGB, gl.UNSIGNED_BYTE, pixels
+        );
+        // Render
+        twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+        twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLE_STRIP);
+        // Ready for next frame
+        requestAnimationFrame(update);
+        frame++;
+    };
+    
+    requestAnimationFrame(update);
+}
+
+window.onload = begin;
